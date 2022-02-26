a2c5fc4b57702e07b1a2016516706869aa33aae0
lamek2005 <lucalev20@gmail.com>
2022-02-26 08:56:49 -0500

    commit from eclipse

---
+++ core lab/src/genlab/labtest.java
@@ -0,0 +1,17 @@
+package genlab;
+
+public class labtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("print something");
+		System.out.println("print something");
+		System.out.println("print something");
+		System.out.println("print something");
+		System.out.println("print something");
+		System.out.println("print something");
+		
+	}
+
+}
